3b852e81ea5df8ee1a7f46fbda647adc9a80cdea
DWijnhorst <72123065+DWijnhorst@users.noreply.github.com>
2022-11-21 11:21:32 +0100

    small test script for running experiments

--- Sudoku_solver_test.py
+++ Sudoku_solver_test.py
@@ -2,7 +2,7 @@ import os
 import json
 import math
 strategy = "-S2"
-for i in range(1,1000):
-    input_file = "input/9x9/test{}.in".format(i)
+for i in range(1,2):
+    input_file = "input/16x16/test{}.in".format(i)
     command  = "python3 SAT.py {} {}".format(strategy, input_file)
     os.system(command)
